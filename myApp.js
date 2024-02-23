9255b23eb8864ef80f9eefaa6bde721e5ae13d11
kanto-taniguchi <kanto.jobs@gmail.com>
2024-02-23 18:15:58 +0900

    UPDATE

--- myApp.js
+++ myApp.js
@@ -1,51 +1,7 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet());
 
 module.exports = app;
 const api = require('./server.js');
@@ -55,7 +11,8 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+
+let port = process.env.PORT || 3000;  // ランダムに割り当てられるポートにも対応
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`👹TEST APP!! ${port}`);
 });
