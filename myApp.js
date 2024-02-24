1a43b8afcfc6d2eab7f00a6a4383adf3efaa6b3d
kanto-taniguchi <kanto.jobs@gmail.com>
2024-02-24 16:10:39 +0900

    UPDATE

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-app.use(helmet());
+app.use(helmet.hidePoweredBy());
 
 module.exports = app;
 const api = require('./server.js');
